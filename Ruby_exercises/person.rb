4c3c755dbcbddaa5ec5ea8ef33732dc37d31dbb7
carol <carolmurithi1994@gmail.com>
2016-09-16 18:51:14 +0300

    add ruby files

---
+++ Ruby_exercises/person.rb
@@ -0,0 +1,13 @@
+class Person
+	attr_accessor :age, :name
+def name
+ print "my name is carol"
+end
+def age
+print "i am 20 years old"
+end
+end
+#calling methods
+carol = Person.new
+carol.name
+carol.age
