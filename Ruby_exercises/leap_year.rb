fc21ca68fe8c35897ba1dd67ff7b241210e4c760
carol <carolmurithi1994@gmail.com>
2016-09-17 18:12:30 +0300

    display leap years

--- Ruby_exercises/leap_year.rb
+++ Ruby_exercises/leap_year.rb
@@ -1,11 +1,13 @@
-#asks for a start of the year.
 puts "what is the starting year?"
-gets
-year = gets.chomp.to_i
-
-#asks the ending of an year.
+start = gets.chomp
 puts "what is the ending year?"
-gets
-year = gets.chomp.to_i
-#
-
+last = gets.chomp
+start.to_i.upto(last.to_i) do |n|
+  if n%4== 0 #its a leap year if divisible by 4
+    if n%100==0 && n%400==0 #its divisible by 100 and 400 then leap year
+      puts "#{n} is a leap year"
+    end
+  else #its not divisible by four
+    puts "#{n} is not a leap year"
+  end
+end
