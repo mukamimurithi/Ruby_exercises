93d51306a4b22e036fa22b62644a6fd0c4d04e4b
carol <carolmurithi1994@gmail.com>
2016-09-21 18:25:27 +0300

    make classmates array and sorting

--- Ruby_exercises/classmates.rb
+++ Ruby_exercises/classmates.rb
@@ -1,10 +1,9 @@
 # one enter their name
-name =gets.chomp 
-name =["jane","mercy","clinton","wycliffe"]
-name.pop
-puts "please enter your name"
-unless name == nil
-  puts "enter second name"
-else
-  puts "end"
+puts "please enter name"
+name = []
+until input == "end\n"
+  input = get.chomp
+  name.push input unless input =="end"
 end
+puts "the sorted list is as follows"
+name.sort
