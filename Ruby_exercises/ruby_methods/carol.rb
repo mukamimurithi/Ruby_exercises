538905872b33433af77cd320e9833036aabf1bd0
carol <carolmurithi1994@gmail.com>
2016-09-26 12:03:09 +0300

    creating a new class object

---
+++ Ruby_exercises/ruby_methods/carol.rb
@@ -0,0 +1,14 @@
+class Person
+def name(name1,name2)
+  @name1 = name1
+  @name2 = name2
+  puts @name1
+  puts @name2
+end
+def full_name
+  puts @name1 +" "+@name2
+end
+end
+carol = Person.new
+carol.name("carol","murithi")
+carol.full_name
